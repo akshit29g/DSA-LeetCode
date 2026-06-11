6d0b49752ee8374adfb3f3920ebed0fd2e461c28
akshit29g <146960347+akshit29g@users.noreply.github.com>
2026-06-12 03:36:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (38.00%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n<=1) return n;
+        int curr;
+        int prev2=0;
+        int prev=1;
+        for (int i=2;i<=n;i++){
+            curr= prev + prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
