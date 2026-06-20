65ed84bcff0831909d8eba3d25d64da274a58ccb
akshit29g <146960347+akshit29g@users.noreply.github.com>
2026-06-21 04:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (72.96%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -13,17 +13,30 @@ class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if (root==NULL) return ans;
         stack<TreeNode*> st;
-        st.push(root);
-        while (!st.empty()){
-            TreeNode* node= st.top();
-            st.pop();
-            ans.push_back(node->val);
-            if (node->left) st.push(node->left);
-            if (node->right) st.push(node->right);
+        TreeNode* curr= root;
+
+        while (curr!=NULL || !st.empty()){
+            if (curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                TreeNode* temp= st.top()->right;
+                if (temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+
+                    while (!st.empty()&& temp==st.top()->right){
+                        temp= st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else curr= temp;
+            }
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
