6a17671db7017df95b50cc06d5307a7c6fb7388b
akshit29g <146960347+akshit29g@users.noreply.github.com>
2026-07-28 02:11:05 +0530

    Time: 15 ms (7.46%) | Memory: 13.4 MB (85.48%) - LeetSync

---
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n= nums.size();
+        int maxi= INT_MIN;
+        for (int i=0;i<n;i++){
+            for (int j=1;j<n;j++){
+                if (j==i) continue;
+                maxi= max(maxi, (nums[i]-1)*(nums[j]-1));
+            }
+        }
+        return maxi;
+    }
+};
