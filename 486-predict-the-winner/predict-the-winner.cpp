eddceaf12994ede7b8f525be30edfdf96005f397
akshit29g <146960347+akshit29g@users.noreply.github.com>
2026-08-01 16:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (16.44%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -12,7 +12,18 @@ public:
     }
     bool predictTheWinner(vector<int>& nums) {
         int n= nums.size();
-        vector<vector<int>> dp(n, vector<int> (n,INT_MIN));
-        return solve(0,n-1,nums,dp)>=0;
+        vector<vector<int>> dp(n, vector<int> (n));
+        for (int i=0;i<n;i++){
+            dp[i][i]= nums[i];
+        }
+        for (int len=2;len<=n;len++){
+            for (int i=0;i+len-1<n;i++){
+                int j= i+len-1;
+
+                dp[i][j]= max(nums[i]-dp[i+1][j], nums[j]-dp[i][j-1]);
+            }
+        }
+
+        return dp[0][n-1]>=0;
     }
 };
